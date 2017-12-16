c0b470de7cc5ed5881b5ef01d9c7deafe16904b4
Anthony <anthonyjlower@gmail.com>
2017-12-15 20:54:36 -0600

    Commit 2 - It's all about Me done

--- js/app.js
+++ js/app.js
@@ -11,3 +11,18 @@ const clicker = {
 }
 
 
+//My Information
+const myInformation = {
+	myName: 'Anthony',
+	age: 27,
+	fact: 'my favorite color is orange',
+	statement() {
+		return "My name is " + this.myName + ", " + this.fact + ", " + "and in 10 years I will be " + parseInt(this.age+10, 10) + " years old";
+	},
+	getOlder(){
+		this.age ++;
+	},
+	changeName() {
+		newName = prompt("Please reenter your name");
+	}
+}
