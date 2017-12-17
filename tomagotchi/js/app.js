b28eff90722d7e550fc2510f6250b0cffcaf425a
Anthony <anthonyjlower@gmail.com>
2017-12-17 17:51:18 -0600

    Commit 12 - Made a Tamagotchi object

---
+++ tomagotchi/js/app.js
@@ -0,0 +1,13 @@
+
+
+//Tamogotchi Object
+var tamagotchi1 = {
+  name: "Bruce",
+  creatureType: "Grizzly Bear",
+  foodInTummy: 100,
+  restedness: 100,
+  health: 100,
+  cry() {
+  	console.log("Whaaaaa!")
+  }
+}
