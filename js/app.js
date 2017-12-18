ef07335af02da83427df987d351d17a6ea20c1c0
Anthony <anthonyjlower@gmail.com>
2017-12-17 19:22:28 -0600

    Commit 24 - Worked on Hungry for More (Iterators)

--- js/app.js
+++ js/app.js
@@ -170,6 +170,61 @@ const totalSheered = sheepShearers.map((person) =>{
 	return a+b
 })
 
+// HUNGRY FOR MORE
+
+//For Each
+
+const gizmo = {start: 10, end: 6, location: "remote", type: "wdi"};
+
+//get an array of all of the keys in the object gizmo
+const key = Object.keys(gizmo)
+//iterate through the array of keys
+key.forEach((item)=>{
+	//print the value of each attribute
+	console.log(gizmo[item])
+})
+
+
+//Reduce
+
+const newArr = [[8, 8], ["eight", "eight"], ["ate"]];
+
+
+const flattenedArr = newArr.reduce((a, b) => {
+	return a.concat(b)
+},[])
+console.log(flattenedArr)
+
+
+
+//Chaining
+
+const stocks = [
+    { symbol: "XFX", price: 240.22, volume: 23432 },
+    { symbol: "TNZ", price: 332.19, volume: 234 },
+    { symbol: "JXJ", price: 120.22, volume: 5323 }
+];
+
+
+let filteredStockSymbols = stocks.filter((stockTicker)=>{
+	return stockTicker.price >= 150
+}).map((goodStocks)=> {
+	return goodStocks.symbol
+}).forEach((goodStockSymbol)=> {
+	console.log(goodStockSymbol)
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
