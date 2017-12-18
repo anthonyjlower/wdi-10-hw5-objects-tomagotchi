bee18a636331c6a6c0e18af6b177fe570e735fa0
Anthony <anthonyjlower@gmail.com>
2017-12-17 18:01:32 -0600

    Commit 16 - made a 2nd Tamogatchi

--- tomagotchi/js/app.js
+++ tomagotchi/js/app.js
@@ -19,3 +19,23 @@ var tamagotchi1 = {
   	console.log(this.name + ": Yaaaawwwnnn!");
   }
 }
+
+
+var tamagotchi2 = {
+  name: "Lucy",
+  creatureType: "Mountain Dog",
+  foodInTummy: 75,
+  restedness: 75,
+  health: 65,
+  cry() {
+  	console.log(this.name + ": Whaaaaa!");
+  	this.foodInTummy -= 25;
+  	console.log(this.name + " has " + this.foodInTummy + " much food in their tummy.")
+  },
+  puke() {
+  	console.log(this.name + ': ":O ~~~~"');
+  },
+  yawn() {
+  	console.log(this.name + ": Yaaaawwwnnn!");
+  }
+}
